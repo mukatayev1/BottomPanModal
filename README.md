eef3343f2e4149c6597522b0cc45d0fff6ff41f8
Aidos Mukatayev <53441647+mukatayev1@users.noreply.github.com>
2023-01-14 17:21:46 +0900

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# BottomPanModal
+
+A description of this package.
---
+++ Tests/BottomPanModalTests/BottomPanModalTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import BottomPanModal
+
+final class BottomPanModalTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(BottomPanModal().text, "Hello, World!")
+    }
+}
